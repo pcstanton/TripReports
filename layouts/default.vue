fbb8dd215ee6064af62d28e9e4c20f2c6d95499c
PCS Montis <pcsmontis@outlook.com>
2019-09-03 11:53:32 -0700

    building new repo

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,56 @@
 <template>
   <div>
+
+
+    <b-navbar toggleable="lg" type="success" variant="info">
+      <b-navbar-brand href="#">NavBar</b-navbar-brand>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item>
+            <b-link to="/">Home</b-link>
+          </b-nav-item>
+          <b-nav-item>
+            <b-link to="/images">Images</b-link>
+          </b-nav-item>
+          <b-nav-item>
+            <b-link to="/accordian">Accordian</b-link>
+          </b-nav-item>
+          <b-nav-item>
+            <b-link to="/grid">Grid</b-link>
+          </b-nav-item> 
+        </b-navbar-nav>
+      </b-collapse>
+      <div class="d-flex justify-content-center text-danger">
+          <slots-header></slots-header>
+      </div>
+    </b-navbar> 
+<!--    
     <nav>
       <nuxt-link to="/">Home</nuxt-link>
       <nuxt-link to="/grid">Grid</nuxt-link>
       <nuxt-link to="/images">Images</nuxt-link>
       <nuxt-link to="/accordian">Accordian</nuxt-link>
     </nav>
-
+-->
     <nuxt />
   </div>
 </template>
+
+<script>
+import SlotsHeader from '@/components/SlotsHeader.vue';
+import { upper }  from '@/components';
+export default {
+  name: "app",
+  components: {
+    SlotsHeader
+  },
+  filters: {
+    upper
+  },
+ 
+};
+</script>
 
 <style>
 html {
